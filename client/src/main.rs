5ad48d6b1e2787f93d9c6d9a13051436beb2f4d3
James ABIB <james.bidule@gmail.com>
2023-11-02 16:04:45 +0100

    refacto: Update Julia Set parameters and error handling
    The main.rs file in the client directory was modified to update the parameters for generating the Julia Set image. The complex number used was changed to (-0.9, 0.27015), and the resolution was increased to 500. Additionally, error handling was added when saving the image, displaying an appropriate message if there is an error during the process.
    
    This commit improves the generation of Julia Set images by adjusting parameters and enhancing error handling.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,22 +1,16 @@
 mod julia;
 use complex::complex_operations::ComplexOperations;
-use shared::types::complex::Complex;
-use shared::types::julia_descriptor::JuliaDescriptor;
-use shared::types::resolution::Resolution;
+use shared::types::{complex::Complex, julia_descriptor::JuliaDescriptor, resolution::Resolution};
 use crate::julia::generate_julia_set;
 use complex::julia_descriptor_impl::JuliaOperations;
 
 fn main() {
-    let c = Complex::new(0.285, 0.013 );
-    let descriptor = JuliaDescriptor::new(c.clone(), 4.0, 200);
+    let c = Complex::new(-0.9, 0.27015);
+    let descriptor = JuliaDescriptor::new(c, 4.0, 500);
     let resolution = Resolution { nx: 800, ny: 800 };
 
-    let img = generate_julia_set(&descriptor, &resolution);
-    if img.save("target/julia.png").is_ok() {
-        println!("L'image du Julia Set a été sauvegardée !");
+    match generate_julia_set(&descriptor, &resolution).save("target/julia.png") {
+        Ok(_) => println!("L'image du Julia Set a été sauvegardée !"),
+        Err(e) => println!("Erreur lors de la sauvegarde de l'image du Julia Set : {}", e),
     }
-    else{
-        println!("L'image du Julia Set n'a pas été sauvegardée !");
-    }
-
 }
