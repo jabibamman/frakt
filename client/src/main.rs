d4186a0aa6a6011d185f1e910d6a71c287e4a9e1
James ABIB <james.bidule@gmail.com>
2023-11-02 11:12:50 +0100

    feat: implement the calc for the generability of the Julia fractal
    - Updated version to 0.1.1 in Cargo.toml
    - Added dependencies for image, complex, and shared in client/Cargo.toml
    - Implemented julia function in client/src/main.rs to generate Julia fractals
    - Added ComplexOperations trait and implemented methods in complex/src/complex_impl.rs
    - Updated dependencies for shared, complex, and server in complex/Cargo.toml and server/Cargo.toml
    - Added JuliaDescriptor struct and JuliaDescriptorOperations trait in shared/src/types/julia_descriptor.rs

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,47 @@
+mod calculations;
+use complex::complex_impl::ComplexOperations;
+use image::{ImageBuffer, Rgb};
+use shared::types::complex::Complex;
+
+fn julia(
+    c: Complex,
+    max_iterations: u32,
+    width: u32,
+    height: u32,
+) -> ImageBuffer<Rgb<u8>, Vec<u8>> {
+    let mut img = ImageBuffer::new(width, height);
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let zx = 4.0 * (x as f64) / (width as f64) - 2.0;
+        let zy = 4.0 * (y as f64) / (height as f64) - 2.0;
+        let mut z = Complex::new(zx, zy);
+        let mut i = 0;
+        while i < max_iterations && z.norm() <= 2.0 {
+            z = z.square().add(&c);
+            i += 1;
+        }
+
+        let color = if i < max_iterations {
+            let log_zn = z.norm().ln();
+            let nu = log_zn / 2.0f64.ln();
+            let iteration_ratio = (i as f64 + 1.0 - nu) / max_iterations as f64;
+            Rgb([
+                (255.0 * iteration_ratio) as u8,
+                (100.0 * iteration_ratio) as u8,
+                (0.0 * iteration_ratio) as u8,
+            ])
+        } else {
+            Rgb([0, 0, 0])
+        };
+
+        *pixel = color;
+    }
+    img
+}
+
 fn main() {
     println!("[CLIENT] Hello, world!");
+    let c2 = Complex::new(-0.9, 0.27015);
+    let img = julia(c2.clone(), 100, 800, 800);
+    img.save("target/julia.png").unwrap();
+    println!("Valeur du complexe : {:?}", c2);
 }
