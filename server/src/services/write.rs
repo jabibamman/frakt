71b045886ba36da59e0add622fac41f3126cafbb
Restyled.io <commits@restyled.io>
2023-11-15 13:27:24 +0000

    Restyled by rustfmt

--- server/src/services/write.rs
+++ server/src/services/write.rs
@@ -1,19 +1,18 @@
 use std::io::Result;
-use std::net::TcpStream;
 use std::io::Write;
+use std::net::TcpStream;
 
 /// Write a string to a stream
-/// 
+///
 /// # Arguments
-/// 
+///
 /// * `stream` - TcpStream
 /// * `message` - String
-/// 
+///
 /// # Return
-/// 
+///
 /// * `Result<TcpStream, std::io::Error>` - TcpStream
 pub fn write(mut stream: TcpStream, message: &str) -> Result<TcpStream> {
-
     stream.write(format!("${}", message).as_bytes())?;
 
     Ok(stream)
