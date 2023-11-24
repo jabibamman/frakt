03e70fd67445e7ecd4b929ddd794d4ad04a9706a
Restyled.io <commits@restyled.io>
2023-11-24 13:46:49 +0000

    Restyled by rustfmt

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,13 +1,15 @@
-use cli::{parser::{CliServerArgs, Parser, CliArgs}, operation::parse_to_address};
+use cli::{
+    operation::parse_to_address,
+    parser::{CliArgs, CliServerArgs, Parser},
+};
 use server::services::server_runner::run_server;
-
 
 fn main() -> std::io::Result<()> {
     let cli_args: CliArgs = CliArgs::Server(CliServerArgs::parse());
     let address = parse_to_address(cli_args);
     match run_server(address.as_str()) {
         Ok(_) => println!("[SERVER] Server stopped."),
-        Err(e) => println!("[SERVER] Server stopped with error: {}", e)
+        Err(e) => println!("[SERVER] Server stopped with error: {}", e),
     }
 
     Ok(())
