bcb9fb10cff372d9fb0591f961ed5e4afb309b21
samiraseddar <seddar20191@gmail.com>
2023-11-26 22:17:28 +0100

    changes fractal_op....

--- complex/src/newtonraphsonz_descriptor_impl.rs
+++ complex/src/newtonraphsonz_descriptor_impl.rs
@@ -2,12 +2,44 @@ use crate::complex_operations::ComplexOperations;
 use shared::types::complex::Complex;
 use shared::types::fractal_descriptor::NewtonRaphsonZ3Descriptor;
 use shared::types::fractal_descriptor::NewtonRaphsonZ4Descriptor;
+use crate::fractal_operations::FractalOperations;
 
 pub trait NewtonRaphsonOperations {
     fn new() -> Self;
     fn pz(&self,z: &Complex) -> Complex;
     fn pz_prime(&self, z:&Complex) -> Complex;
-    fn iterate_complex_point(&self, complex_point: &Complex) -> u16;
+}
+impl FractalOperations for NewtonRaphsonZ3Descriptor {
+    fn iterate_complex_point(&self, complex_point: &Complex,  max_iteration: u16) -> u16 {
+        let mut z0 = Complex::new(1.0, 0.0);
+        let mut iterations = 0;
+        let mut z1 = z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
+        let e: f64 = std::f64::consts::E;
+        while z1 .sub(&z0).abs().sqrt()>= 1.0 * e - 6.0  {
+            z0=z1;
+            z1=z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
+            iterations += 1;
+        }
+
+        iterations
+    }
+
+}
+impl FractalOperations for NewtonRaphsonZ4Descriptor {
+    fn iterate_complex_point(&self, complex_point: &Complex , max_iteration: u16) -> u16 {
+        let mut z0 = Complex::new(1.0, 0.0);
+        let mut iterations = 0;
+        let mut z1 = z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
+        let e: f64 = std::f64::consts::E;
+        while z1.sub(&z0).abs().sqrt()>= 1.0 * e - 6.0 {
+            z0=z1;
+            z1=z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
+            iterations += 1;
+        }
+
+        iterations
+    }
+
 }
 
 impl NewtonRaphsonOperations for NewtonRaphsonZ3Descriptor {
@@ -30,20 +62,6 @@ impl NewtonRaphsonOperations for NewtonRaphsonZ3Descriptor {
 
     }
 
-
-    fn iterate_complex_point(&self, complex_point: &Complex, ) -> u16 {
-        let mut z0 = Complex::new(1.0, 0.0);
-        let mut iterations = 0;
-        let mut z1 = z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-        let e: f64 = std::f64::consts::E;
-      while z1 .sub(&z0).abs().sqrt()>= 1.0 * e - 6.0  {
-            z0=z1;
-            z1=z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-            iterations += 1;
-        }
-
-        iterations
-    }
 }
 impl NewtonRaphsonOperations for NewtonRaphsonZ4Descriptor {
     fn new() -> Self {
@@ -65,19 +83,7 @@ impl NewtonRaphsonOperations for NewtonRaphsonZ4Descriptor {
 
     }
 
-    fn iterate_complex_point(&self, complex_point: &Complex, ) -> u16 {
-        let mut z0 = Complex::new(1.0, 0.0);
-        let mut iterations = 0;
-        let mut z1 = z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-        let e: f64 = std::f64::consts::E;
-        while z1.sub(&z0).abs().sqrt()>= 1.0 * e - 6.0 {
-            z0=z1;
-            z1=z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-            iterations += 1;
-        }
 
-        iterations
-    }
 
 }
 
