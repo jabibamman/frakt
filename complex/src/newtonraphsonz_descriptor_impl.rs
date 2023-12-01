3af9dcd184ac851b1667a0a48576391569668048
James ABIB <james.bidule@gmail.com>
2023-12-01 13:51:08 +0100

    fix: Newton-Raphson Z3 iteration
    This commit fixes the implementation of the `iterate_complex_point` function in the `NewtonRaphsonZ3Descriptor` struct. The changes include:
    - Improved documentation for the function, providing details on its purpose and parameters.
    - Refactored variable names for clarity and consistency.
    - Added a convergence threshold to determine when the iterative process has sufficiently converged.
    - Utilized Newton's method for root-finding of the function `z^3 - 1`.
    - Improved handling of convergence conditions.
    
    The commit also includes minor changes to the `Complex` struct in terms of adding the `Copy` trait to improve code readability.

--- complex/src/newtonraphsonz_descriptor_impl.rs
+++ complex/src/newtonraphsonz_descriptor_impl.rs
@@ -53,23 +53,53 @@ impl NewtonRaphsonOperations for NewtonRaphsonZ4Descriptor {
     }
 }
 impl FractalOperations for NewtonRaphsonZ3Descriptor {
-    fn iterate_complex_point(&self, complex_point: &Complex,  _max_iteration: u16) -> u16 {
-        let mut z0 = Complex::new(1.0, 0.0);
-        let mut iterations = 0;
-        let mut z1 = z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-        let e: f64 = std::f64::consts::E;
-        let max = 256;
-        while ((z1 .sub(&z0)).abs()*(z1 .sub(&z0)).abs())>= 1.0 * e - 6.0 && iterations<max {
-            z0=z1;
-            z1=z0.sub(&self.pz(&z0).div(self.pz_prime(&z0))).add(complex_point);
-            iterations += 1;
+    /// Iterates over a complex point.
+    ///
+    /// This function takes a reference to a `Complex` number and an iteration limit (`_max_iteration`).
+    /// It iterates the complex point using a specific mathematical process (likely related to Newton's method
+    /// for finding roots of a complex function), and returns the number of iterations it took to converge
+    /// or reach the maximum iterations.
+    ///
+    /// # Parameters
+    /// * `complex_point` - A reference to the complex number to iterate over.
+    /// * `max_iteration` - The maximum number of iterations to perform.
+    ///
+    /// # Returns
+    /// * `u16` - The number of iterations it took for the process to converge, or the maximum iteration
+    /// limit if convergence did not occur.
+    ///
+    /// # Notes
+    /// * The function utilizes a convergence threshold to determine when the iterative process
+    /// has sufficiently converged. 
+    /// * It uses Newton's method for root-finding of the function `z^3 - 1`.
+    fn iterate_complex_point(&self, complex_point: &Complex,  max_iteration: u16) -> u16 {
+        let mut z = *complex_point;
+        let mut iteration = 0u16;
+        let convergence_threshold = 1e-12; // carré de 1e-6 pour la comparaison directe avec magnitude_squared
+
+        while iteration < max_iteration {
+            let fz = z.square().mul(&z).sub(&Complex::new(1.0, 0.0)); // z^3 - 1
+            let dfz = z.square().mul(&Complex::new(3.0, 0.0)); // 3z^2
+
+            if dfz.magnitude_squared() < convergence_threshold {
+                break;
+            }
+
+            let fz_over_dfz = fz.div(dfz); // f(z) / f'(z)
+            let new_z = z.sub(&fz_over_dfz); // z - f(z) / f'(z)
+
+            if new_z.sub(&z).magnitude_squared() < convergence_threshold {
+                break;
+            }
+
+            z = new_z; // z = z - f(z) / f'(z)
+            iteration += 1;
         }
-        //println!("la variable es : {}",iterations);
 
-        iterations
+        iteration
     }
-
 }
+
 impl FractalOperations for NewtonRaphsonZ4Descriptor {
     fn iterate_complex_point(&self, complex_point: &Complex , _max_iteration: u16) -> u16 {
         let mut z0 = Complex::new(1.0, 0.0);
@@ -84,9 +114,10 @@ impl FractalOperations for NewtonRaphsonZ4Descriptor {
             //println!("la valeur de z0 est {} {}",z0.im,z0.re);
             //println!("la valeur de z1 est {} {}",z1.im,z1.re);
             iterations += 1;
-            //println!("{}",iterations);
+            println!("{}",iterations);
         }
 
+        println!("la variable es : {}",iterations);
         iterations
     }
 
--- shared/src/types/complex.rs
+++ shared/src/types/complex.rs
@@ -3,7 +3,7 @@
 /// # Attributes
 /// * `re` - The real part of the complex number, represented as a `f64`.
 /// * `im` - The imaginary part of the complex number, also a `f64`.
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Copy)]
 pub struct Complex {
     pub re: f64,
     pub im: f64,
