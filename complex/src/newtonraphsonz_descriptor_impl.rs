cf82e97dd4af161c30bc28626563633092e45608
James ABIB <james.bidule@gmail.com>
2024-01-06 14:25:01 +0100

    refacto: delete panic clean code

--- complex/src/newtonraphsonz_descriptor_impl.rs
+++ complex/src/newtonraphsonz_descriptor_impl.rs
@@ -43,7 +43,11 @@ impl FractalOperations for NewtonRaphsonZ3Descriptor {
         let mut iterations = 0;
 
         while iterations < max_iteration {
-            let next_z = newton_raphson_step(&z, 3);
+            let next_z = match newton_raphson_step(&z, 3) {
+                Ok(z) => z,
+                Err(_) => break,
+            };
+            
             if (next_z.sub(&z)).magnitude_squared() < 1e-6 {
                 break;
             }
@@ -78,7 +82,11 @@ impl FractalOperations for NewtonRaphsonZ4Descriptor {
         let mut iterations = 0;
 
         while iterations < max_iteration {
-            let next_z = newton_raphson_step(&z, 4);
+            let next_z = match newton_raphson_step(&z, 4) {
+                Ok(z) => z,
+                Err(_) => break,
+            };
+
             if (next_z.sub(&z)).magnitude_squared() < 1e-6 {
                 break;
             }
@@ -103,20 +111,20 @@ impl FractalOperations for NewtonRaphsonZ4Descriptor {
 /// # Returns
 ///
 /// Le prochain point complexe dans l'itération pour NewtonRaphsonZ3.
-fn newton_raphson_step(z: &Complex, degree: u32) -> Complex {
+fn newton_raphson_step(z: &Complex, degree: u32) -> Result<Complex, &'static str> {
     match degree {
         3 => {
             // p(z) = z^3 - 1 et p'(z) = 3z^2
             let pz = z.square().mul(z).sub(&Complex::new(1.0, 0.0));
             let dpz = z.square().mul(&Complex::new(3.0, 0.0));
-            z.sub(&pz.div(dpz))
+            Ok(z.sub(&pz.div(dpz)))
         }
         4 => {
             //  p(z) = z^4 - 1 et p'(z) = 4z^3
             let pz = z.square().square().sub(&Complex::new(1.0, 0.0));
             let dpz = z.square().mul(z).mul(&Complex::new(4.0, 0.0));
-            z.sub(&pz.div(dpz))
+            Ok(z.sub(&pz.div(dpz)))
         }
-        _ => panic!("Unsupported polynomial degree"),
+        _ => Err("Degree must be 3 or 4")
     }
 }
